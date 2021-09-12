b97a010c60b7f742ca8f26dc59be6376dd4997d6
sarscov2 <2698892343@qq.com>
2021-09-13 06:57:13 +0800

    5-2 集成axios npm install axios@0.21.0 --save

--- src/main/java/com/jiawa/wiki/config/CorsConfig.java
+++ src/main/java/com/jiawa/wiki/config/CorsConfig.java
@@ -10,11 +10,13 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        //表示针对所有的请求地址
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedHeaders(CorsConfiguration.ALL)
                 .allowedMethods(CorsConfiguration.ALL)
                 .allowCredentials(true)
+                //在调用电子书接口之前，前端会先发一个OPTIONS请求，用来检查这个接口是否存在，检查成功，会再次发一个正式的请求
                 .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
     }
 
