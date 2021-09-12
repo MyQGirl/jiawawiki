69ba5106e96df057868d8a3d7cfbde8d09ff8832
sarscov2 <2698892343@qq.com>
2021-09-12 23:00:30 +0800

    4-7 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,21 @@
   <a-layout>
     <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-    甲蛙电子书
-    </a-layout-footer>
+<!--    //这个是在组件文件本身中声明的名字-->
+<the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
   import TheHeader from '@/components/the-header.vue'; // @ is an alias to /src
+  import TheFooter from '@/components/the-footer.vue'; // @ is an alias to /src
 
   export default defineComponent({
     name: 'app',
     components: {
       TheHeader,
+      TheFooter,
     },
   });
 </script>
