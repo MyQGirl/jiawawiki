d0162f8a3cd4ae28aa40f920aaefd9f78faf7a39
sarscov2 <2698892343@qq.com>
2021-09-16 00:08:27 +0800

    5-4 集成图标组件 npm install @ant-design/icons-vue@5.1.8 --save

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-layout>
+  <a-layout id="components-layout-demo-top-side-2">
     <the-header></the-header>
     <router-view/>
 <!--    //这个是在组件文件本身中声明的名字-->
@@ -21,21 +21,4 @@
   });
 </script>
 
-<style>
-  #components-layout-demo-top-side .logo {
-    float: left;
-    width: 120px;
-    height: 31px;
-    margin: 16px 24px 16px 0;
-    background: rgba(255, 255, 255, 0.3);
-  }
 
-  .ant-row-rtl #components-layout-demo-top-side .logo {
-    float: right;
-    margin: 16px 0 16px 24px;
-  }
-
-  .site-layout-background {
-    background: #fff;
-  }
-</style>
--- web/src/main.ts
+++ web/src/main.ts
@@ -5,6 +5,15 @@ import store from './store'
 import Antd from 'ant-design-vue';
 
 import 'ant-design-vue/dist/antd.css';
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+
+import * as Icons from '@ant-design/icons-vue';
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+
+// 全局使用图标
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
 
 
