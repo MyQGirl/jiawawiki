ae6e6be82446fd59a38965b212a2c1ddfe877d7e
sarscov2 <2698892343@qq.com>
2021-09-16 23:30:21 +0800

    5-4 电子书布局调整 一行显示三列电子书

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,88 +1,84 @@
 <template>
     <a-layout>
-        <a-layout-sider width="200" style="background: #fff">
-            <a-menu
-                    mode="inline"
-                    v-model:selectedKeys="selectedKeys2"
-                    v-model:openKeys="openKeys"
-                    :style="{ height: '100%', borderRight: 0 }"
-            >
-                <a-sub-menu key="sub1">
-                    <template #title>
+
+
+            <a-layout-sider width="200" style="background: #fff">
+                <a-menu
+                        mode="inline"
+                        v-model:selectedKeys="selectedKeys2"
+                        v-model:openKeys="openKeys"
+                        :style="{ height: '100%', borderRight: 0 }"
+                >
+                    <a-sub-menu key="sub1">
+                        <template #title>
               <span>
                 <user-outlined />
                 subnav 1
               </span>
-                    </template>
-                    <a-menu-item key="1">option1</a-menu-item>
-                    <a-menu-item key="2">option2</a-menu-item>
-                    <a-menu-item key="3">option3</a-menu-item>
-                    <a-menu-item key="4">option4</a-menu-item>
-                </a-sub-menu>
-                <a-sub-menu key="sub2">
-                    <template #title>
+                        </template>
+                        <a-menu-item key="1">option1</a-menu-item>
+                        <a-menu-item key="2">option2</a-menu-item>
+                        <a-menu-item key="3">option3</a-menu-item>
+                        <a-menu-item key="4">option4</a-menu-item>
+                    </a-sub-menu>
+                    <a-sub-menu key="sub2">
+                        <template #title>
               <span>
                 <laptop-outlined />
                 subnav 2
               </span>
-                    </template>
-                    <a-menu-item key="5">option5</a-menu-item>
-                    <a-menu-item key="6">option6</a-menu-item>
-                    <a-menu-item key="7">option7</a-menu-item>
-                    <a-menu-item key="8">option8</a-menu-item>
-                </a-sub-menu>
-                <a-sub-menu key="sub3">
-                    <template #title>
+                        </template>
+                        <a-menu-item key="5">option5</a-menu-item>
+                        <a-menu-item key="6">option6</a-menu-item>
+                        <a-menu-item key="7">option7</a-menu-item>
+                        <a-menu-item key="8">option8</a-menu-item>
+                    </a-sub-menu>
+                    <a-sub-menu key="sub3">
+                        <template #title>
               <span>
                 <notification-outlined />
                 subnav 3
               </span>
-                    </template>
-                    <a-menu-item key="9">option9</a-menu-item>
-                    <a-menu-item key="10">option10</a-menu-item>
-                    <a-menu-item key="11">option11</a-menu-item>
-                    <a-menu-item key="12">option12</a-menu-item>
-                </a-sub-menu>
-            </a-menu>
-        </a-layout-sider>
+                        </template>
+                        <a-menu-item key="9">option9</a-menu-item>
+                        <a-menu-item key="10">option10</a-menu-item>
+                        <a-menu-item key="11">option11</a-menu-item>
+                        <a-menu-item key="12">option12</a-menu-item>
+                    </a-sub-menu>
+                </a-menu>
+            </a-layout-sider>
 
-        <a-layout-content
-                :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-        >
-            <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-                <template #footer>
-                    <div>
-                        <b>ant design vue</b>
-                        footer part
-                    </div>
-                </template>
-                <template #renderItem="{ item }">
-                    <a-list-item key="item.title">
-                        <template #actions>
+            <a-layout-content
+                    :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+            >
+                <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }"
+                       :data-source="ebooks">
+
+                    <template #renderItem="{ item }">
+
+                        <a-list-item key="item.name">
+
+                            <template #actions>
           <span v-for="{ type, text } in actions" :key="type">
             <component v-bind:is="type" style="margin-right: 8px" />
             {{ text }}
           </span>
-                        </template>
-                        <template #extra>
-                            <img
-                                    width="272"
-                                    alt="logo"
-                                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                            />
-                        </template>
-                        <a-list-item-meta :description="item.description">
-                            <template #title>
-                                <a :href="item.href">{{ item.title }}</a>
                             </template>
-                            <template #avatar><a-avatar :src="item.avatar" /></template>
-                        </a-list-item-meta>
-                        {{ item.content }}
-                    </a-list-item>
-                </template>
-            </a-list>
-        </a-layout-content>
+
+                            <a-list-item-meta :description="item.description">
+                                <template #title>
+                                    <a :href="item.href">{{ item.name }}</a>
+                                </template>
+                                <template #avatar><a-avatar :src="item.cover" /></template>
+                            </a-list-item-meta>
+
+                        </a-list-item>
+                    </template>
+                </a-list>
+            </a-layout-content>
+
     </a-layout>
+
 </template>
 
 
@@ -151,3 +147,13 @@ export default defineComponent({
 
 });
 </script>
+
+<style scoped>
+    .ant-avatar {
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 8%;
+        margin: 5px 0;
+    }
+</style>
