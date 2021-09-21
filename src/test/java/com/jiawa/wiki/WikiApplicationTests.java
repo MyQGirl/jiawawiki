76791a396149ce6a2e00675b0ee932ff788c3af5
sarscov2 <2698892343@qq.com>
2021-09-21 12:24:43 +0800

    5-5 Vue多环境配置，增加了dev和prod配置文件 编译和启动命令增加多环境

--- src/test/java/com/jiawa/wiki/WikiApplicationTests.java
+++ src/test/java/com/jiawa/wiki/WikiApplicationTests.java
@@ -1,13 +1,29 @@
+package com.jiawa.wiki;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.ObjectUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 // package com.jiawa.wiki;
 //
 // import org.junit.jupiter.api.Test;
 // import org.springframework.boot.test.context.SpringBootTest;
 //
 // @SpringBootTest
-// class WikiApplicationTests {
-//
-//     @Test
-//     void contextLoads() {
-//     }
-//
-// }
+ public  class WikiApplicationTests {
+
+
+     @Test
+     void contextLoads() {
+         String test = new String();
+         Map<String, JsonNode> map  = new HashMap<>();
+        Boolean flag =  ObjectUtils.isEmpty(map);
+         System.out.println(flag);
+
+
+     }
+
+ }
