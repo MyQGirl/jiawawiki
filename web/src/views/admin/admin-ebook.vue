3c4b42e5c21b3bbb3b720f3108fbb1438e47d524
sarscov2 <2698892343@qq.com>
2021-09-28 20:05:32 +0800

    6-6 电子书管理页面前后端分页整合完成

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -116,12 +116,19 @@
       const handleQuery = (params: any) => {
         loading.value = true;
         // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
-        axios.get("/ebook/list", params).then((response)=>{
+        axios.get("/ebook/list", {
+          params: {
+            page: params.page,
+            size: params.size
+
+        }
+        }).then((response)=>{
           loading.value = false;
           const data = response.data;
-            ebooks.value = data.content;
+            ebooks.value = data.content.list;
             // 重置分页按钮
             pagination.value.current = params.page;
+            pagination.value.total = data.content.total;
         });
       };
 
@@ -137,7 +144,10 @@
       };
 
       onMounted(() => {
-        handleQuery({});
+        handleQuery({
+          page: 1,
+          size: pagination.value.pageSize
+        });
       });
 
       return {
