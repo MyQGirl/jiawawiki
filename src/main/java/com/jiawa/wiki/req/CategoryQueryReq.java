38bb0492dc379b6d5097d35ecb71ab588eeebd67
sarscov2 <2698892343@qq.com>
2021-10-28 23:05:30 +0800

    7-3 完成分类管理基本的增删改查功能

---
+++ src/main/java/com/jiawa/wiki/req/CategoryQueryReq.java
@@ -0,0 +1,9 @@
+package com.jiawa.wiki.req;
+
+public class CategoryQueryReq extends PageReq {
+    @Override
+    public String toString() {
+        return "CategoryQueryReq{} " + super.toString();
+    }
+
+}
