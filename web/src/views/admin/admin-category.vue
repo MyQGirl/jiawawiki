b6a7eb05317ff428b4fbf820d3cd3bb714706028
sarscov2 <2698892343@qq.com>
2021-10-31 16:06:53 +0800

    7-4 分类管理不显示分页

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -7,11 +7,7 @@
       <p>
         <a-form layout="inline" :model="param">
           <a-form-item>
-            <a-input v-model:value="param.name" placeholder="名称">
-            </a-input>
-          </a-form-item>
-          <a-form-item>
-            <a-button type="primary" @click="handleQuery({page: 1, size: pagination.pageSize})">
+            <a-button type="primary" @click="handleQuery()">
               查询
             </a-button>
           </a-form-item>
@@ -27,9 +23,8 @@
               :columns="columns"
               :row-key="record => record.id"
               :data-source="categorys"
-              :pagination="pagination"
               :loading="loading"
-              @change="handleTableChange"
+              :pagination="false"
       >
         <template #cover="{ text: cover }">
           <img v-if="cover" :src="cover" alt="avatar" />
@@ -95,11 +90,7 @@
       const param = ref();
       param.value = {};
       const categorys = ref();
-      const pagination = ref({
-        current: 1,
-        pageSize: 10,
-        total: 0
-      });
+
       const loading = ref(false);
 
       const columns = [
@@ -128,41 +119,21 @@
       /**
        * 数据查询
        **/
-      const handleQuery = (params: any) => {
+      const handleQuery = () => {
         loading.value = true;
         // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
-        axios.get("/category/list", {
-          params: {
-            page: params.page,
-            size: params.size,
-            name: param.value.name
+        axios.get("/category/all").then((response)=>{
+        loading.value = false;
+        const data = response.data;
+        if(data.success){
 
+          categorys.value = data.content;
+        } else{
+          message.error(data.message);
         }
-        }).then((response)=>{
-          loading.value = false;
-          const data = response.data;
-          if(data.success){
+      });
+    };
 
-            categorys.value = data.content.list;
-            // 重置分页按钮
-            pagination.value.current = params.page;
-            pagination.value.total = data.content.total;
-          } else{
-            message.error(data.message);
-          }
-        });
-      };
-
-      /**
-       * 表格点击页码时触发
-       */
-      const handleTableChange = (pagination: any) => {
-        console.log("看看自带的分页参数都有啥：" + pagination);
-        handleQuery({
-          page: pagination.current,
-          size: pagination.pageSize
-        });
-      };
 
 
 // -------- 表单 ---------
@@ -185,10 +156,7 @@
 
 
             // 重新加载列表
-            handleQuery({
-              page: pagination.value.current,
-              size: pagination.value.pageSize,
-            });
+            handleQuery();
           } else {
             message.error(data.message);
           }
@@ -219,29 +187,23 @@
           if (data.success) {
 
             // 重新加载列表
-            handleQuery({
-              page: pagination.value.current,
-              size: pagination.value.pageSize,
-            });
+            handleQuery();
           }
         })
       };
 
 
       onMounted(() => {
-        handleQuery({
-          page: 1,
-          size: pagination.value.pageSize
-        });
+        handleQuery();
       });
 
       return {
         categorys,
         param,
-        pagination,
+
         columns,
         loading,
-        handleTableChange,
+
 
         edit,
         add,
